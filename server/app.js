7c80840a636598b49a6f548f5a18c45b8eaf5073
Lahoucine Ijioui <ije1work@gmail.com>
2024-03-06 13:14:23 +0100

    initial socketio server

--- server/app.js
+++ server/app.js
@@ -1,12 +1,34 @@
-import { config } from "dotenv";
-config();
 import express from "express";
-
 const app = express();
+import { Server } from "socket.io";
 
-app.get("/", (req, res) => {
-    res.send('ok')
+
+const server = app.listen(4000, () => {
+    console.log("Running On 4000");
 })
 
-const PORT = process.env.PORT
-app.listen(PORT, console.log("Server Running on 3001"));
+
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+        credentials: false
+    }
+})
+
+io.on('connection', (socket) => {
+    console.log("connected", socket.id);
+
+    socket.on('join_room', (data) => {
+        socket.join(data)
+        console.log(`User with id ${socket.id} joined room ${data}`);
+    })
+
+    socket.on('send_message', (data) => {
+        socket.to(data.room).emit("recieve_message", data)
+    })
+
+    socket.on('disconnect', () => {
+        console.log("disconnected");
+    })
+})
