4e579de0c0562ab7ee0e6cef89a684ccd1198a36
Lahoucine Ijioui <ije1work@gmail.com>
2024-03-06 13:10:21 +0100

    set routes and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Chat from './Pages/Chat';
+import Join from './Pages/Join';
+import Main from './Pages/Main';
 
 
 function App() {
-
   return (
-    <h1>Chat App with React Node SocketIo</h1>
+    <Routes>
+      <Route path='/' element={<Main />} />
+      <Route path='/join' element={<Join />} />
+      <Route path='/chat' element={<Chat />} />
+    </Routes>
   )
 }
 
---
+++ src/Pages/Join.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Join = () => {
+  return (
+    <div>Join Page</div>
+  )
+}
+
+export default Join;
